7d19334d584cca212f058f1cd174e0c978d7ee7e
Rick Clark <rkingsleyclark@gmail.com>
2017-02-15 16:24:54 +0000

    Completed creatNewNote route

--- public/app.js
+++ public/app.js
@@ -1,15 +1,39 @@
 (function(exports) {
 
   function App() {
-    // I am a constructor
-  };
+    var self = this;
 
-  //Add prototype functions
-  App.prototype.getHome = function() {
-    console.log("in getHome");
-  };
+    this.getHome = function() {
+      console.log("in getHome");
+    };
+
+    this.handleHashChange = function(){
+      if (location.hash === "#createNewNote") {
+        self.createNewNote();
+        self.clearForm();
+        location.hash = "#home";
+      }
+    };
+
+    this.createNewNote = function(){
+      var noteText = document.getElementById('new_note_textarea').value;
+      var newNote = new note(noteText);
+      noteList.pushNote(newNote);
+    };
+
+    this.clearForm = function() {
+      var element = document.getElementById('new_note_textarea');
+      element.value = "";
+    };
+  }
+
 
   //Export app constructor to window
   exports.app = App;
 
 })(this);
+
+var notesApp = new app();
+var noteList = new noteList();
+//EVENTS
+window.onhashchange = notesApp.handleHashChange;
