cb1af6d1cf79cb9588ffce88a370dec966934850
Enrico Graziani <mrenrich84@gmail.com>
2017-02-16 14:49:52 +0000

    Refactored isEqual, toContain and their not versions, toThrow as well;  toNotThrow needs refactoring

--- public/test/spec/tddFrameworkSpec.js
+++ public/test/spec/tddFrameworkSpec.js
@@ -1,37 +1,37 @@
-describe("describe(): organize example groups",function(){});
-
-describe("it(): stores each example",function(){
-  it("executes lines of code and displays a message when assert is failing",function(){
-    assert(true).isEqual(false);
-  });
-
-  it("when assert doesn't fail, shows nothing",function(){
-    assert(true).isEqual(true);
-  });
-
-  function isElementHidden(id) {
-    var element = document.getElementById(id);
-    var elementClass = element.getAttribute('class');
-    return checkIfHidden(elementClass);
-  }
-
-  it("saves body state upon launching and restore it when everything finish",function(){
-    var id = "note_form";
-    var elementOutsideTestState = isElementHidden(id); // false
-    it("hiddensetter() toggles hidden class from element",function(){
-      var elementInsideTestState = isElementHidden(id); // false
-      hiddenSetter(id);
-      var elementInsideTestNewState = isElementHidden(id); // true
-      assert(elementInsideTestNewState).isEqual(!elementInsideTestState);
-    });
-    var elementOutsideTestNewState = isElementHidden(id); // false
-    assert(elementOutsideTestNewState).isEqual(elementOutsideTestState);
-  });
-});
+// describe("describe(): organize example groups",function(){});
+//
+// describe("it(): stores each example",function(){
+//   it("executes lines of code and displays a message when assert is failing",function(){
+//     assert(true).isEqual(false);
+//   });
+//
+//   it("when assert doesn't fail, shows nothing",function(){
+//     assert(true).isEqual(true);
+//   });
+//
+//   function isElementHidden(id) {
+//     var element = document.getElementById(id);
+//     var elementClass = element.getAttribute('class');
+//     return checkIfHidden(elementClass);
+//   }
+//
+//   it("saves body state upon launching and restore it when everything finish",function(){
+//     var id = "note_form";
+//     var elementOutsideTestState = isElementHidden(id); // false
+//     it("hiddensetter() toggles hidden class from element",function(){
+//       var elementInsideTestState = isElementHidden(id); // false
+//       hiddenSetter(id);
+//       var elementInsideTestNewState = isElementHidden(id); // true
+//       assert(elementInsideTestNewState).isEqual(!elementInsideTestState);
+//     });
+//     var elementOutsideTestNewState = isElementHidden(id); // false
+//     assert(elementOutsideTestNewState).isEqual(elementOutsideTestState);
+//   });
+// });
 
 describe('assert(): interface to tests',function(){
   it('.isEqual(): test if expectation are === to what it has been given',function(){
-    assert(1+1).isEqual(2);
+    assert(2).isEqual(2);
   });
   it('.isNotEqual(): reverts .isEqual',function(){
     assert(1+1).isNotEqual(3);
