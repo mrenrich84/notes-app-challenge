d0206543f8d96f8e8f05a9d705ee4551cfa7cc07
Rick Clark <rkingsleyclark@gmail.com>
2017-02-16 14:30:19 +0000

    Added createNewForm test and refactored app injection of notelist

--- public/test/spec/appSpec.js
+++ public/test/spec/appSpec.js
@@ -1,12 +1,27 @@
-describe("clearForm", function(){
-  var testApp = new app();
+describe("ClearForm", function(){
   var testNoteList = new noteList();
+  var testApp = new app(testNoteList);
+
 
   it("clears the content of the new_note_textarea div", function(){
     var element = document.getElementById('new_note_textarea');
     element.value = "Test";
     testApp.clearForm();
     assert(document.getElementById('new_note_textarea').value).isEqual("");
+  });
+
+});
+
+
+describe("CreateNewNote", function(){
+  var testNoteList = new noteList();
+  var testApp = new app(testNoteList);
+
+  it("creates a new Note object and adds to the noteList", function(){
+    var element = document.getElementById('new_note_textarea');
+    element.value = "Test";
+    testApp.createNewNote();
+    assert(testApp.appNoteList.getNoteById(0).getText()).isEqual("Test");
   });
 
 });
