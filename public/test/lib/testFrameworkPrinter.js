0ac50d697811304b41e8390a2878c80fed5f1716
Rick Clark <rkingsleyclark@gmail.com>
2017-02-17 15:41:20 +0000

    Refactored testFrameworkPrinter

--- public/test/lib/testFrameworkPrinter.js
+++ public/test/lib/testFrameworkPrinter.js
@@ -1,62 +1,68 @@
 (function(exports){
 
-  var testPrinter = {};
-
-  testPrinter.printTestHeader = function(args){
-    indentation = {
-      describe : "",
-      it : "  "
-    };
-    style = {
-      describe : 'font-weight: bold; color: black',
-      it : 'color: black'
-    };
-
-    var testHeaderType = args.testHeaderType;
-    var message = args.message;
-    console.log('%c' + indentation[testHeaderType] + message, style[testHeaderType]);
+  var RESULTS_ERROR_OUTCOMES = {
+    isEqual: {
+      expected :  'Expected: ',
+      got :       'Got:      '
+    },
+    isNotEqual: {
+      expected :  'Expected not to be: ',
+      got :       'Got:                '
+    },
+    toContain: {
+      expected :  'String to be contained:',
+      got :       'The whole string is:   '
+    },
+    toNotContain: {
+      expected :  'String not to be contained: ',
+      got :       'The whole string is:        '
+    },
+    toThrow: {
+      expected :  'Expected: ',
+      got :       'Got:      '
+    },
+    toNotThrow: {
+      expected :  'No error message expected:          ',
+      got :       'But instead got this error message: '
+    }
   };
 
-  testPrinter.printAssertResults = function(args){
-    errorOutcomes = {
-      isEqual: {
-        expected :  'Expected: ',
-        got :       'Got:      '
-      },
-      isNotEqual: {
-        expected :  'Expected not to be: ',
-        got :       'Got:                '
-      },
-      toContain: {
-        expected :  'String to be contained:',
-        got :       'The whole string is:   '
-      },
-      toNotContain: {
-        expected :  'String not to be contained: ',
-        got :       'The whole string is:        '
-      },
-      toThrow: {
-        expected :  'Expected: ',
-        got :       'Got:      '
-      },
-      toNotThrow: {
-        expected :  'No error message expected:          ',
-        got :       'But instead got this error message: '
-      }
-    };
-
-    indentation = {
-      mainLevel : "  ".repeat(2),
-      subLevel : "  ".repeat(5)
-    };
-
-    var matcher = args.matcherType;
-    message = indentation.mainLevel + "Failure/Error: while checking " + args.matcherType + "\n" +
-              indentation.subLevel + errorOutcomes[matcher].expected  + args.expectation + "\n" +
-              indentation.subLevel + errorOutcomes[matcher].got + args.assertion;
-
-    console.error(message);
+  var RESULTS_INDENTATION = {
+    mainLevel : "  ".repeat(2),
+    subLevel : "  ".repeat(5)
   };
+
+  var HEADER_INDENTATION = {
+    describe : "",
+    it : "  "
+  };
+
+  var HEADER_STYLE = {
+    describe : 'font-weight: bold; color: black',
+    it : 'color: black'
+  };
+
+
+
+  var testPrinter = {
+
+    printTestHeader : function(args){
+      var testHeaderType = args.testHeaderType;
+      var message = args.message;
+      console.log('%c' + HEADER_INDENTATION[testHeaderType] + message, HEADER_STYLE[testHeaderType]);
+    },
+
+    printAssertResults : function(args){
+      var matcher = args.matcherType;
+      message = RESULTS_INDENTATION.mainLevel + "Failure/Error: while checking " + args.matcherType + "\n" +
+                RESULTS_INDENTATION.subLevel + RESULTS_ERROR_OUTCOMES[matcher].expected  + args.expectation + "\n" +
+                RESULTS_INDENTATION.subLevel + RESULTS_ERROR_OUTCOMES[matcher].got + args.assertion;
+
+      console.error(message);
+    }
+  };
+
+
 
   exports.testPrinter = testPrinter;
 
